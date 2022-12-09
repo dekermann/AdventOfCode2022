a12cf2193b52fe31e3b52717fc307b62a5c98550
David Ekermann <davidekermann89@hotmail.com>
2022-12-09 11:07:37 +0100

    WIP d9

---
+++ 09/David/solution.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("input", "utf-8");
+const rows = input.split("\r\n");
+
+function followTarget(currentPosition, targetPosition) {
+    let moves = [];
+    let delta = [targetPosition[0] - currentPosition[0], targetPosition[1] - currentPosition[1]];
+    if ((Math.abs(delta[0]) > 0 && Math.abs(delta[1]) > 1) ||  (Math.abs(delta[0]) > 1 && Math.abs(delta[1]) > 0)) { // Move Diagonal first
+        currentPosition[0] += Math.sign(delta[0]);
+        currentPosition[1] += Math.sign(delta[1]);
+        moves.push([...currentPosition]);
+    }
+    delta = [targetPosition[0] - currentPosition[0], targetPosition[1] - currentPosition[1]];
+    for (let i = 1; i < Math.abs(delta[0]); ++i) {
+        currentPosition[0] += Math.sign(delta[0]);
+        moves.push([...currentPosition]);
+    }
+    for (let i = 1; i < Math.abs(delta[1]); ++i) {
+        currentPosition[1] += Math.sign(delta[1]);
+        moves.push([...currentPosition]);
+    }
+    return moves;
+}
+
+function snake(input, knots) {
+    const visitedPositions = new Map();
+    visitedPositions.set(0 + ':' + 0, true);
+
+    let currentPositions = [...new Array(knots)].map(() => [...new Array(2)].map(() => 0));
+    for (let i = 0; i < input.length; ++i) {
+        let move = input[i].split(' ');
+        switch (move[0]) {
+            case 'U':
+                currentPositions[0][0] += parseInt(move[1]);
+                break;
+            case 'D':
+                currentPositions[0][0] -= parseInt(move[1]);
+                break;
+            case 'L':
+                currentPositions[0][1] -= parseInt(move[1]);
+                break;
+            case 'R':
+                currentPositions[0][1] += parseInt(move[1]);
+                break;
+        }
+
+        let moves = [];
+        for (let i = 0; i < knots - 1; ++i) {
+            moves = followTarget(currentPositions[i + 1], currentPositions[i]);
+        }
+        for (let i = 0; i < moves.length; ++i) {
+            visitedPositions.set(moves[i][0] + ":" + moves[i][1], true);
+        }
+    }
+
+    for (let i = 15; i > -15; --i) {
+        let line = "";
+        for (let j = -15; j < 15; ++j) {
+            if (visitedPositions.has(i + ":" + j)) line += "#"
+            else line += "."
+        }
+        console.log(line)
+    }
+
+    return visitedPositions.size;
+}
+
+// 2696 too high
+console.log(`Total rows ${rows.length}`);
+console.log(`part 1 > ${snake(rows, 2)}`);
+console.log(`part 2 > ${snake(rows, 10)}`);
